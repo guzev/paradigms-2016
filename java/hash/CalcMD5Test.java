645ccde8d2541d9516a0cab7b6e051b1b8151d91
Georgiy Korneev <kgeorgiy@keorgiy.info>
2016-02-11 15:01:58 +0300

    Random tests added

--- java/hash/CalcMD5Test.java
+++ java/hash/CalcMD5Test.java
@@ -16,7 +16,11 @@ public class CalcMD5Test {
                 Triple.of("__test_hello.txt", "hello", "5D41402ABC4B2A76B9719D911017C592"),
                 Triple.of("__test_empty.txt", "", "D41D8CD98F00B204E9800998ECF8427E"),
                 Triple.of("__test_hash.txt", "D41D8CD98F00B204E9800998ECF8427E", "80404D0C6D24E87F650FF7D1985CD762"),
-                Triple.of("__test spaces txt", "this text contains spaces", "0AACC55D27AB04E26F3C7CCCAAC7B902")
+                Triple.of("__test spaces txt", "this text contains spaces", "0AACC55D27AB04E26F3C7CCCAAC7B902"),
+                "DB1FE12FBF369B59635FFCBA22007FF7",
+                "664F95A728D5A0B117F3E5344B3B8ACE",
+                "77FA15192315F12E6121B9EC6B04EB54",
+                "DE1AF6F09647AB7B4E8821F0C7918195"
         ).printStatus();
     }
 }
--- java/hash/HashChecker.java
+++ java/hash/HashChecker.java
@@ -4,7 +4,9 @@ import common.MainChecker;
 import common.Triple;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 import java.util.stream.Collectors;
 
 /**
@@ -38,7 +40,8 @@ public class HashChecker extends MainChecker {
             final Triple<String, String, String> hello,
             final Triple<String, String, String> empty,
             final Triple<String, String, String> hash,
-            final Triple<String, String, String> spaces
+            final Triple<String, String, String> spaces,
+            final String... random
     ) throws IOException {
         test("input.txt", input);
         test("input.txt", hello);
@@ -48,6 +51,21 @@ public class HashChecker extends MainChecker {
         test("input.txt", hello, empty, hash, spaces);
         test("input.txt", empty, empty, empty);
         test("__test__input", hello, empty, hash, empty, spaces);
+        final List<Triple<String, String, String>> randoms = new ArrayList<>();
+        for (int i = 10, j = 0; i <= 10_000_000; i *= 100, j++) {
+            randoms.add(Triple.of("__test__random" + i, randomString(i), random[j]));
+        }
+        @SuppressWarnings("unchecked")
+        final Triple<String, String, String>[] randomTriples = randoms.stream().toArray(Triple[]::new);
+        test("__test__random", randomTriples);
         return this;
+    }
+
+    private String randomString(final int length) {
+        final StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < length; i++) {
+            sb.append((char) random.nextInt());
+        }
+        return sb.toString();
     }
 }
